a0b19ea3018203477165dcf0dbd58a357bea0eae
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-18 13:10:58 -0400

    Implement form validation

---
+++ script.js
@@ -0,0 +1,29 @@
+let email = document.querySelector("input[type='email']");
+let phone = document.querySelector("input[type='tel']");
+
+let password = document.querySelector("input[type='password']");
+let confirmPassword = document.querySelector("#confirmPassword");
+
+email.addEventListener('change',emailOrPhone);
+phone.addEventListener('change',emailOrPhone);
+confirmPassword.addEventListener('change',checkPassword);
+
+function emailOrPhone(){
+  email.setAttribute('required',true);
+  phone.setAttribute('required',true);
+
+  if(email.checkValidity()){
+    phone.removeAttribute('required');
+  }else if(phone.checkValidity()){
+    phone.removeAttribute('required');
+  }
+}
+
+function checkPassword(){
+  if(confirmPassword.value != password.value){
+    confirmPassword.setCustomValidity("Password does not match");
+    confirmPassword.reportValidity();
+  }else{
+    confirm.setCustomValidity();
+  }
+}
